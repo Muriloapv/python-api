c1fb3eb968c4616f5090ec16e572b1d989f89fd7
muriloapv <muriloalves121@outlook.com>
2024-10-21 22:24:13 -0300

    root get criada

---
+++ README.md
@@ -0,0 +1,4 @@
+Para iniciar o docker: docker-compose up --build -d 
+Para finalizar:        docker-compose down
+Para reiniciar: docker-compose restart
+Para ter o endereco do docker: docker ps
--- app/main.py
+++ app/main.py
@@ -2,3 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+#oque deve ter apos o localHost:8000, seu eu deixar @app.get('/'), ele vai cair na raiz apenas com o localhost8000
+#ex: https://localhost:8000/product
+@app.get("/")
+def root():
+    return{"mensagem" : "hello world."}
+#         {chave      : valor         }
